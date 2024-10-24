f8472dd21c09da743d680c949b8e3eb387070a4b
ad5322 <arundeepak966@gmail.com>
2024-10-24 15:49:01 +0530

    listview

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
+
+
 def home(request):
     return render(request,'home.html')
-# Create your views here.
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
