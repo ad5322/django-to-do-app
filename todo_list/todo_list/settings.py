61cbf066738f66b22b26f31d51e5780286d13eb5
ad5322 <arundeepak966@gmail.com>
2024-10-26 21:58:38 +0530

    login

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'todo',
     'bootstrap5',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,4 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = 'login'
